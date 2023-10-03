fd1bbffc258c4a95b2730d488f3d73208cd124ee
Pradeep Das <pradeepdasxyz@gmail.com>
2023-10-03 09:52:50 +0530

    first commit

---
+++ src/automate/autoscript.java
@@ -0,0 +1,11 @@
+package automate;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
+public class autoscript {
+	public static void main (String[] args) {
+         ChromiumDriver driver = new ChromeDriver();
+	} 
+
+}
